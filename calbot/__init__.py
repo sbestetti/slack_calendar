7ead9a15fe0e86ace40217e01554ff288d85aa01
Sergio Bestetti <sbestetti@gmail.com>
2018-09-10 16:00:06 +0100

    Form data handler

--- calbot/__init__.py
+++ calbot/__init__.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, json, request
+from werkzeug.datastructures import CombinedMultiDict, MultiDict
 
 def create_app():
     
     app = Flask(__name__)
 
-    @app.route('/')
+    @app.route('/', methods=['POST'])
     def index():
-        return 'App running'
+        user_id = request.form['user_id'].strip()
+        user_name = request.form['user_name'].strip()
+        text = request.form['text'].strip()
+        channel_name = request.form['channel_name'].strip()
+        channel_id = request.form['channel_id'].strip()
+        print("User {} ({}) posted '{}' on channel {} ({})".format(user_name, user_id, text, channel_name, channel_id))
+        return 'Got it', 202
+
+    @app.route('/status')
+    def test():
+        return 'App running', 200
 
     return app
