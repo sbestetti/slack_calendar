6352dbb7c5a7c351608395cb0c15d9c0f70b4162
Sergio Bestetti <sbestetti@gmail.com>
2018-09-12 13:46:15 +0100

    Response handler working

--- calbot/__init__.py
+++ calbot/__init__.py
@@ -1,19 +1,13 @@
 from flask import Flask, json, request
 from werkzeug.datastructures import CombinedMultiDict, MultiDict
+import sys
 
 def create_app():
     
     app = Flask(__name__)
 
     @app.route('/', methods=['POST'])
-    def index():
-        #How to get data from the Slack form:
-        #user_id = request.form['user_id'].strip()
-        #user_name = request.form['user_name'].strip()
-        #text = request.form['text'].strip()
-        #channel_name = request.form['channel_name'].strip()
-        #channel_id = request.form['channel_id'].strip()
-        #response_to_send = "User {} ({}) posted '{}' on channel {} ({})".format(user_name, user_id, text, channel_name, channel_id)        
+    def index():                
         slack_buttons = '{"text": "What can I do for you?","attachments": [{"text": "Get me a free room right now in","fallback": "Something went wrong :(","callback_id": "free_rooms","color": "#3AA3E3","attachment_type": "default","actions": [{"name": "free_rooms","text": "Dublin","type": "button","value": "dub"},{"name": "free_rooms","text": "Wexford","type": "button","value": "wex"}]}]}'
         response = app.response_class(
             response=slack_buttons,
@@ -22,6 +16,19 @@ def create_app():
         )
         return response
 
+    @app.route('/response_manager', methods=['POST'])
+    def freerooms():
+        form_json = json.loads(request.form['payload'])
+        selection = form_json['actions'][0]['value']
+        print(selection)
+        response_json = '{"text": "' + selection + '"}'
+        response = app.response_class(
+            response=response_json,
+            status=200,
+            mimetype='application/json'
+        )        
+        return response
+    
     @app.route('/status')
     def test():
         return 'App running', 200
