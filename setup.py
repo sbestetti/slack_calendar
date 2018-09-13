c53bb9b55a02916d67c79d12e5e7abcada1f3325
Sergio Bestetti <sbestetti@gmail.com>
2018-09-13 19:01:21 +0100

    API working. Missing Slack responses

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='calbot',
     version='0.1',
+    author="Sergio Bestetti",
+    author_email="sergio@bestetti.net",
+    description="Google Calendar bot for Slack",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
