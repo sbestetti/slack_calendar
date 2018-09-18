0b0f987e1faa88e4c18d0c9e79fdb27953a4b19b
Sergio B <sbestetti@gmail.com>
2018-09-18 09:39:51 +0100

    Correct message when no rooms are available

--- calbot/responsebuilder.py
+++ calbot/responsebuilder.py
@@ -5,17 +5,26 @@ import json
 def respond_to_query(selection, response_address):
     rooms_to_check = roommanager.get_rooms(selection)
     free_rooms = eventmanager.check_if_free(rooms_to_check)
-    payload = {
-        "text": "These rooms are free for the next 30 minutes:",
-        "attachments": [
-            {
-                "text": "",                
-            }
-        ]
-    }
-
-    for item in free_rooms:
-        payload['attachments'][0]['text'] = payload['attachments'][0]['text'] + item.name + '\n'
+    if len(free_rooms) == 0:
+        payload = {
+            "text": "Sorry, no free rooms right now. :(",
+            "attachments": [
+                {
+                    "text": "",                
+                }
+            ]
+        }
+    else:
+        payload = {
+            "text": "These rooms are free for the next 30 minutes:",
+            "attachments": [
+                {
+                    "text": "",                
+                }
+            ]
+        }
+        for item in free_rooms:
+            payload['attachments'][0]['text'] = payload['attachments'][0]['text'] + item.name + '\n'
     
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'} 
     r = requests.post(response_address, data=json.dumps(payload), headers=headers)
